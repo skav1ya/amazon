6ec6c618451f2a06fbfb18aa79bce28fc18cbfea
skav1ya <kaviyasureshkumar2@gmail.com>
2023-01-19 19:37:20 +0530

    first commit

---
+++ amazon/frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header c>
+        <a href="/">amazon</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
